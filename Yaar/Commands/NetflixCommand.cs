650fca1092f6a5ba7c428956e452ad4ed24e2d17
Taimur <taimur38@gmail.com>
2012-07-24 02:16:50 -0400

    browser view

---
+++ Yaar/Commands/NetflixCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Yaar.Listeners;
+using Yaar.Locale;
+using Yaar.Views;
+
+namespace Yaar.Commands
+{
+    class NetflixCommand : ICommand
+    {
+        public string Handle(string input, Match match, IListener listener)
+        {
+            BrowserView.Create("http://www.netflix.com");
+            return Speech.Yes.Parse();
+        }
+
+        public string Regexes { get { return "netflix"; } }
+    }
+}
---
+++ Yaar/Views/BrowserView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Yaar.Views
+{
+    /// <summary>
+    /// Interaction logic for BrowserView.xaml
+    /// </summary>
+    public partial class BrowserView : Window
+    {
+        public static void Create(string url)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+                {
+                    var view = new BrowserView(url) {Left = 10, Top = 10};
+                    view.Show();
+                });
+        }
+
+        public BrowserView(string url)
+        {
+            InitializeComponent();
+            Browser.Loaded += (sender, args) => Browser.Navigate(new Uri(url));
+        }
+    }
+}
