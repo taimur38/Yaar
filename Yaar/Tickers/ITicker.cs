1ae57d674df41a94c7fdb67e03f20103ff04aaa4
Taimur <taimur38@gmail.com>
2012-07-24 01:23:20 -0400

    fixed merging

---
+++ Yaar/Tickers/ITicker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yaar.Tickers
+{
+    interface ITicker
+    {
+        void Start();
+        void Stop();
+    }
+}
